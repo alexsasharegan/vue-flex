26a40a7453df03641a3fe0c50e32064c982ddc89
Alex Regan <alex.regan@artisancolour.com>
2017-06-20 09:45:31 -0700

    vue-flex component build-out

---
+++ lib/components/Flex.vue
@@ -0,0 +1,18 @@
+<template>
+    <component :is="tag"
+               :class="flexClassList">
+        <slot></slot>
+    </component>
+</template>
+
+<style lang="scss">
+    @import "../styles/flex";
+</style>
+
+<script>
+import flexPropsMixin from '../mixins/flexProps'
+
+export default {
+    mixins: [flexPropsMixin],
+}
+</script>
